47cc0b03a6ff7ead8a702a5951a3acb6bad9fe97
assistant1277 <shivarajghangale2002@gmail.com>
2024-09-25 14:40:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using PrimeNumber.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number to check if it is prime number ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Prime.CheckAndPrintPrime(inputNumber);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ model/Prime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber.model
+{
+    public class Prime
+    {
+        public static void CheckAndPrintPrime(int number)
+        {
+            if (number <= 1)
+            {
+                Console.WriteLine($"{number} not a prime number");
+                return;
+            }
+
+            bool isPrime = true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            string resultMessage = isPrime ? $"{number} is a prime number " : $"{number} is not a prime number";
+            Console.WriteLine(resultMessage);
+        }
+    }
+}
